ca03a62257c3b664ebc267ac3901d2f7145bbefc
lindamciver <linda.mciver@jmss.vic.edu.au>
2016-03-11 13:43:47 +1100

    image metadata playing

--- imagedata.py
+++ imagedata.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
  
-def get_exif(fn):
+def get_exif(fn): # opens image "fn" (filename) and extracts and decodes the metadata by converting the tags to a more intelligible form
     ret = {}
     pic = Image.open(fn)
     info = pic._getexif()
@@ -10,10 +10,16 @@ def get_exif(fn):
         ret[decoded] = value
     return ret
 
-tags = get_exif("/Users/lindam/Pictures/fungus.JPG")
+tags = get_exif("/Users/lindam/Pictures/ethanwallace.JPG")
+
+#print (type(tags))
+#print (tags)
+print ("gps: ", type(tags["GPSInfo"]))
+
+#for key,value in tags.items():
+#   print (key,value)
 
 for tag in tags:
     print (tag)
-
 print (tags["GPSInfo"])
 
