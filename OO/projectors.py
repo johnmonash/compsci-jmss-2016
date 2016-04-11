16e5ec86ec9a39d433040ddf2111e2692a1371c9
Victor Rajewski <askvictor@gmail.com>
2016-04-11 14:57:12 +1000

    added projectors OO real things

--- OO/projectors.py
+++ OO/projectors.py
@@ -1,21 +1,84 @@
-class Projector(object):
-    def __init__(self, network_address):
-        self.address = network_address
-        self._power_status = read_from_projector("POWER", network_address)
+import socket
+
+
+def create_projector(setting):
+    klass = getattr(__name__, setting['class'])
+    if 'password' in setting:
+        return klass(setting['address'], setting['password'])
+    else:
+        return klass(setting['address'])
+
+
+def create_projectors(projector_settings):
+    projectors = []
+    for setting in projector_settings:
+        projectors.append(create_projector(setting))
+    return projectors
+
+
+class ProjectorException(Exception):
+    pass
+
+
+class GenericNetworkProjector(object):
+    def __init__(self, address):
+        self.power = None
+        self.source_id = None
 
     def power_on(self):
-        send_command_to_projector(network_address)
-        self._power_status = True
+        self.power = True
 
     def power_off(self):
-        self._power_status = False
-        
+        self.power = False
 
-proj_2b7 = Projector("118.139.125.169")
-proj_1b1 = Projector("118.139.125.122")
+    def set_source(self, source_id):
+        self.source_id = source_id
 
-del proj_2b7
 
-proj_2b7.power_on()
+class EpsonProjector(GenericNetworkProjector):
+    PORT = 3629
+    HANDSHAKE_STRING = 'ESC/VP.net\x10\x03\x00\x00\x00\x00'
+    HANDSHAKE_STRING_PW = 'ESC/VP.net\x10\x03\x00\x00\x00\x01\x01\x01'
+    HANDSHAKE_RESPONSE = b'ESC/VP.net\x10\x03\x00\x00 \x00'
 
-proj_2b7.power_status = True
+    def __init__(self, address, password=None):
+        super().__init__(address)
+
+        self.sock = socket.socket()
+        #self.sock.setblocking(False)
+        self.sock.connect((address, self.PORT))
+        if password:
+            handshake = self.HANDSHAKE_STRING_PW + password
+        else:
+            handshake = self.HANDSHAKE_STRING
+        self.sock.send(bytes(handshake, "UTF-8"))
+        response = self.sock.recv(4096)
+        if response != self.HANDSHAKE_RESPONSE:
+            raise ProjectorException
+        #TODO - read power status and source ?
+
+    def power_on(self):
+        self.sock.send(bytes("PWR ON\r", "UTF-8"))
+        response = self.sock.recv(4096)
+        super().power_on()
+
+    def power_off(self):
+        self.sock.send(bytes("PWR OFF\r", "UTF-8"))
+        response = self.sock.recv(4096)
+        super().power_off()
+
+    def set_source(self, source_id):
+        self.sock.send(bytes("SOURCE {id}\r".format(id=source_id), "UTF-8"))
+        response = self.sock.recv(4096)
+        #TODO - check success
+        super().set_source(source_id)
+
+    def get_source(self):
+        self.sock.send(b"SOURCE?\r")
+        response = self.sock.recv(4096)
+        return response
+        #super().set_source(source_id)
+
+
+    def __del__(self):
+        self.sock.close()
