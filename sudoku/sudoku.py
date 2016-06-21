4b53ee040b6b05cc9437838f21bb33572398b84c
Victor Rajewski <askvictor@gmail.com>
2016-06-21 11:53:30 +1000

    check

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -4,7 +4,7 @@ class SudokuPuzzle:
     VALID_INCOMPLETE = 2
 
     def __init__(self):
-        self.dimension = 3
+        self.dimension = 3  # TODO extend to allow rectangular grids
         self.data = [
             [None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None],
@@ -26,8 +26,17 @@ class SudokuPuzzle:
         for row in self.data:
             col.append(row[x])
 
+    def get_cols(self):
+        cols = []
+        for i in range(self.dimension **2):
+            cols.append(self.get_col(i))
+        return cols
+
     def get_row(self, y):
         return self.data[y]
+
+    def get_rows(self):
+        return self.data
 
     def get_sub_grid(self, x, y):
         "returns a flat list of numbers representing a sub-grid (index from 0, so 0,0 is top left, 2,2 is bottom right)"
@@ -40,30 +49,38 @@ class SudokuPuzzle:
 
         return nums
 
-    def check_nums(self, l):
+    def get_sub_grids(self):
+        grids = []
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                grids.append(i,j)
+        return grids
+
+    def check_list(self, l):
         "checks if a list of numbers is a valid sudoku set. Returns INVALID, COMPLETE, or VALID_INCOMPLETE"
-        result = self.COMPLETE  # DANGEROUS!!!
+        valid = True
+        complete = True
         for i in l:
             if i is None:
-                result = self.VALID_INCOMPLETE
+                complete = False
                 continue
             if l.count(i) > 1:
-                result = self.INVALID
-        return result
+                valid = False
+        return (valid, complete)
 
-    def check_row(self, y):
-        self.check_nums(self.get_row(y))
 
-    def check_col(self, x):
-        self.check_nums(self.get_col(x))
-
-    def check_sub_grid(self, x, y):
-        self.check_nums(self.get_sub_grid(x,y))
-
+    def check_lists(self, ls):
+        valid = True
+        complete = True
+        for l in ls():
+            (valid, complete) = (valid, complete) and self.check_list(l)
+        return (valid, complete)
 
     def check(self):
         "Check if current structure is valid"
-        for
-        # check each row
-        # check each col
-        # check each subgrid
+        valid = True
+        complete = True
+        (valid, complete) = (valid, complete) and self.check_lists(self.get_rows())
+        (valid, complete) = (valid, complete) and self.check_lists(self.get_cols())
+        (valid, complete) = (valid, complete) and self.check_lists(self.get_sub_grids())
+        return (valid, complete)
