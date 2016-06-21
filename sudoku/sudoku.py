6f9ac2d1e82c943763f87c9c7b8032c506c94916
Victor Rajewski <askvictor@gmail.com>
2016-06-21 11:32:46 +1000

    added a bunch of real code

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,5 +1,10 @@
-class sudoku_puzzle:
+class SudokuPuzzle:
+    INVALID = 0
+    COMPLETE = 1
+    VALID_INCOMPLETE = 2
+
     def __init__(self):
+        self.dimension = 3
         self.data = [
             [None, None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None, None],
@@ -25,13 +30,26 @@ class sudoku_puzzle:
         return self.data[y]
 
     def get_sub_grid(self, x, y):
+        "returns a flat list of numbers representing a sub-grid (index from 0, so 0,0 is top left, 2,2 is bottom right)"
+        nums = []
 
+        for i in range(self.dimension):
+            row = self.get_row(y*3 + i)
+            for j in range(self.dimension):
+                nums.append(row[x*3 + j])
 
-    def check_sub_grid(self):
-        pass
+        return nums
 
     def check_nums(self, l):
-
+        "checks if a list of numbers is a valid sudoku set. Returns INVALID, COMPLETE, or VALID_INCOMPLETE"
+        result = self.COMPLETE  # DANGEROUS!!!
+        for i in l:
+            if i is None:
+                result = self.VALID_INCOMPLETE
+                continue
+            if l.count(i) > 1:
+                result = self.INVALID
+        return result
 
     def check_row(self, y):
         self.check_nums(self.get_row(y))
@@ -39,8 +57,13 @@ class sudoku_puzzle:
     def check_col(self, x):
         self.check_nums(self.get_col(x))
 
+    def check_sub_grid(self, x, y):
+        self.check_nums(self.get_sub_grid(x,y))
+
+
     def check(self):
         "Check if current structure is valid"
+        for
         # check each row
         # check each col
         # check each subgrid
